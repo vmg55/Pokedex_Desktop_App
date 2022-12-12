beb71fca9e0d0ae0831f9df1b6b4d3b4bf511bf7
Victor Magdaleno <vmagdaleno55@gmail.com>
2022-12-12 14:10:23 -0800

    App now displays the selected pokemon's stats along with a progress bar. Still only supports first 251 pokemon.

--- pokedex.py
+++ pokedex.py
@@ -31,7 +31,7 @@ class Pokedex:
         self.combobox = ttk.Combobox(self.header_frame, textvariable=self.pokemon_entered)
         self.combobox.config(values=[val.name for key, val in pokedex_dict.items()])
         self.combobox.grid(row=3, column=0, columnspan=2, padx = 20, pady=20)
-        self.combobox.set('Bulbasuar')
+        self.combobox.set('Bulbasaur')
 
         # Frame for pokemon image and typing
         self.picture_frame = ttk.Frame(master)
@@ -49,11 +49,45 @@ class Pokedex:
         # Frame for Pokemon stats
         self.stats_frame = ttk.Frame(master)
         self.stats_frame.pack()
+        # Labels for stats
+        self.hp_label = ttk.Label(self.stats_frame, text=f"Health \t {pokedex_dict['Bulbasaur'].hp}")
+        self.attack_label = ttk.Label(self.stats_frame, text=f"Attack \t {pokedex_dict['Bulbasaur'].attack}")
+        self.defense_label = ttk.Label(self.stats_frame, text=f"Defense \t {pokedex_dict['Bulbasaur'].defense}")
+        self.sp_attack_label = ttk.Label(self.stats_frame, text=f"Sp Att \t {pokedex_dict['Bulbasaur'].sp_attack}")
+        self.sp_defense_label = ttk.Label(self.stats_frame, text=f"Sp Def \t {pokedex_dict['Bulbasaur'].sp_defense}")
+        self.speed_label = ttk.Label(self.stats_frame, text=f"Speed \t {pokedex_dict['Bulbasaur'].speed}")
+        # Label Configurations
+        self.hp_label.grid(row=0, column = 0)
+        self.attack_label.grid(row=1, column=0)
+        self.defense_label.grid(row=2, column=0)
+        self.sp_attack_label.grid(row=3, column=0)
+        self.sp_defense_label.grid(row=4, column=0)
+        self.speed_label.grid(row=5, column=0)
+        # Stat bar
+        self.hp_bar = ttk.Progressbar(self.stats_frame, orient=HORIZONTAL, length=160)
+        self.attack_bar = ttk.Progressbar(self.stats_frame, orient=HORIZONTAL, length=160)
+        self.defense_bar = ttk.Progressbar(self.stats_frame, orient=HORIZONTAL, length=160)
+        self.sp_attack_bar = ttk.Progressbar(self.stats_frame, orient=HORIZONTAL, length=160)
+        self.sp_defense_bar = ttk.Progressbar(self.stats_frame, orient=HORIZONTAL, length=160)
+        self.speed_bar = ttk.Progressbar(self.stats_frame, orient=HORIZONTAL, length=160)
+        # Stat bar configurations
+        self.hp_bar.grid(row=0, column=1)
+        self.attack_bar.grid(row=1, column=1)
+        self.defense_bar.grid(row=2, column=1)
+        self.sp_attack_bar.grid(row=3, column=1)
+        self.sp_defense_bar.grid(row=4, column=1)
+        self.speed_bar.grid(row=5, column=1)
+        self.hp_bar.config(mode='determinate', maximum=255, value=105)
+        self.attack_bar.config(mode='determinate', maximum=160, value=54)
+        self.defense_bar.config(mode='determinate', maximum=160, value=54)
+        self.sp_attack_bar.config(mode='determinate', maximum=160, value=70)
+        self.sp_defense_bar.config(mode='determinate', maximum=160, value=70)
+        self.speed_bar.config(mode='determinate', maximum=160, value=50)
 
         # Event binding for pokemon combobox selection
         self.combobox.bind("<<ComboboxSelected>>", self.get_pokemon)
 
-    # These actions occur when a button is pressed
+    # These actions occur when a pokemon is selcted from the combobox
     def get_pokemon(self, event):
         pokemon_selected = self.combobox.get()
         pokedex_id = pokedex_dict[pokemon_selected].pokedex_id
@@ -62,6 +96,18 @@ class Pokedex:
         self.type2_label.config(text=pokedex_dict[pokemon_selected].type2)
         self.sprite_label.img = poke_sprite
         self.sprite_label.config(image=self.sprite_label.img)
+        self.hp_label.config(text=f"Health \t {pokedex_dict[pokemon_selected].hp}")
+        self.attack_label.config(text=f"Attack \t {pokedex_dict[pokemon_selected].attack}")
+        self.defense_label.config(text=f"Defense \t {pokedex_dict[pokemon_selected].defense}")
+        self.sp_attack_label.config(text=f"Sp Att \t {pokedex_dict[pokemon_selected].sp_attack}")
+        self.sp_defense_label.config(text=f"Sp Def \t {pokedex_dict[pokemon_selected].sp_defense}")
+        self.speed_label.config(text=f"Speed \t {pokedex_dict[pokemon_selected].speed}")
+        self.hp_bar.config(mode='determinate', maximum=255, value=pokedex_dict[pokemon_selected].hp)
+        self.attack_bar.config(mode='determinate', maximum=160, value=pokedex_dict[pokemon_selected].attack)
+        self.defense_bar.config(mode='determinate', maximum=160, value=pokedex_dict[pokemon_selected].defense)
+        self.sp_attack_bar.config(mode='determinate', maximum=160, value=pokedex_dict[pokemon_selected].sp_attack)
+        self.sp_defense_bar.config(mode='determinate', maximum=160, value=pokedex_dict[pokemon_selected].sp_defense)
+        self.speed_bar.config(mode='determinate', maximum=160, value=pokedex_dict[pokemon_selected].speed)
 
 
 pokemon_df = pd.read_csv('Pokemon_data.csv')
@@ -88,3 +134,6 @@ for index, record in pokemon_df.iterrows():
 
     pokedex_dict[name] = pokemon.Pokemon(name, pokedex_id, type1, type2, hp, attack, defense, spAtk,
                                          spDef, speed, gen=generation)
+
+sorted_tuples = sorted(pokedex_dict.items(), key=lambda item: item[1].name)
+sorted_dict = {k: v for k, v in sorted_tuples}
